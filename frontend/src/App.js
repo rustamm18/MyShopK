9c7ce19cd5033a82ef5e0c631f79ec79f12e0ef2
rustamm18 <98675965+rustamm18@users.noreply.github.com>
2022-03-04 19:52:04 -0800

    React header/footer/bootswatch/bootsrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
 //arrow functions
+import {Container} from 'react-bootstrap';
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <>
-      <h1>Welcome to MyShop!</h1>
+      <Header />
+      <main>
+        <Container>
+        <h1>Welcome to MyShop!</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 }
