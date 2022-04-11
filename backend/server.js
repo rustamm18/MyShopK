1fa24a29713c0c3350ec7a912da3e06de08b1425
rustamm18 <98675965+rustamm18@users.noreply.github.com>
2022-04-11 15:55:44 -0700

    Lecture 12 3/13 nodemon, concurrently and setup db

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,15 @@
-const express = require('express')
-const products = require('./data/products')
+import express from 'express'
+import dotenv from 'dotenv'
+import products from './data/products.js'
+import connectDB from './config/db.js'
 
 const app = express()
+dotenv.config()
+connectDB()
+
+app.get('/', (req, res) => {
+    res.json({'msg': 'Hello World'})
+})
 
 app.get('/api/products', (req, res) => {
     res.json(products)
@@ -13,4 +21,5 @@ app.get('/api/products/:id', (req, res) => {
    res.json(product);
 })
 
-app.listen(5000, console.log('Server is running on port 5000'));
+const PORT = process.env.PORT || 5000
+app.listen(PORT, console.log(`Server is running on port ${PORT}`));
