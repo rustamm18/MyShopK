bca1ac45f05186f7c2624402b3da8711cccf52a5
rustamm18 <98675965+rustamm18@users.noreply.github.com>
2022-04-12 08:38:29 -0700

    Model and dummy data 3/19 lecture

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,18 @@
 import mongoose from 'mongoose';
 
 const connectDB = async () => {
-
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-        useUnifiedTopology: true,
-        useNewUrlParser: true
+   try{
+    const conn = await mongoose.connect(process.env.
+        MONGO_URI, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true
 
     })
     console.log(`MongoDb connected: ${conn.connection.host}`)
+} catch(error){
+    console.error(`Error: ${error.message}`)
+    process.exit(1)
+}
 }
 
 export default connectDB
