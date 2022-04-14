b4b6cec71e9baeb6111e750b78903aaea6c4f2e2
rustamm18 <98675965+rustamm18@users.noreply.github.com>
2022-04-14 09:09:22 -0700

    Adding to cart functionality 04/02

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,28 @@
-//arrow functions
 import {Container} from 'react-bootstrap';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import HomeScreen from './screens/HomeScreen';
-import ProductScreen from './screens/ProductScreen';
-import React from 'react'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Footer from "./components/Footer.js";
+import Header from "./components/Header.js";
+import CartScreen from './screens/CartScreen.js';
+import HomeScreen from './screens/HomeScreen.js';
+import ProductScreen from './screens/ProductScreen.js';
 
+//arrow functions
 const App = () => {
   return (
     <>
-    <Router>
-      <Header />
-      <main className='py-3'>
-        <Container>
-        <Routes>
-          <Route path='/' element={<HomeScreen />} exact />
-          <Route path='/product/:id' element={<ProductScreen /
-          >} />
-        </Routes>
-        </Container>
-      </main>
-      <Footer />
+      <Router>
+        <Header />
+        <main className='py-3'>
+          <Container>
+            <Routes>
+              <Route path='/' element={<HomeScreen />} exact />
+              <Route path='/product/:id' element={<ProductScreen />} />
+              <Route path='/cart/:id' element={<CartScreen />} />
+              <Route path='/cart' element={<CartScreen />} />
+             </Routes>
+          </Container>
+        </main>
+        <Footer />
       </Router>
     </>
   );
